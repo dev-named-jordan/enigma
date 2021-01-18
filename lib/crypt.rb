a0cb33656038569f9cdc87da61bb257a31b6d40c
Jordan Beck <jordanfbeck@gmail.com>
2021-01-18 14:27:00 -0700

    Add refactors and tests in messages for 96.09 passing simple_cov

--- lib/crypt.rb
+++ lib/crypt.rb
@@ -1,10 +1,6 @@
   require './lib/generator'
 
 module Cryptable
-  def cleaned_input_message_to_elements(message)
-    message.downcase.strip.split("")
-  end
-
   def date_to_offset_elements(date)
     squared = (date.to_i ** 2)
     squared.to_s[-4..-1].to_i.digits.reverse
@@ -36,16 +32,6 @@ module Cryptable
       alphabet_with_index[letter_element] = index
     end
     alphabet_with_index
-  end
-
-  def original_numbers_for_message(message)
-    cleaned_input_message_to_elements(message).map do |letter|
-      if alphabet_with_index[letter].nil?
-        letter
-      else
-        alphabet_with_index[letter] if alphabet.include?(letter)
-      end
-    end
   end
 
   def number_to_decrypted_array(message, key, date)
